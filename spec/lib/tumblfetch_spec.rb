4f42ddc943b4cf4eb2fd326b13dc42ac9989a0aa
Sensuke Osawa <senosa@gmail.com>
2014-05-14 20:42:00 +0900

    Add Tumblfetch.init expectations (skeletal structure)

--- spec/lib/tumblfetch_spec.rb
+++ spec/lib/tumblfetch_spec.rb
@@ -1,1 +1,18 @@
 require 'spec_helper'
+require_relative '../../lib/tumblfetch'
+
+describe Tumblfetch do
+  describe '.init' do
+    context 'when ~/.tumblr does not exist' do
+      it 'should print message running `tumblr`'
+    end
+
+    context 'when ~/.tumblr exist' do
+      it 'should generate Tumblfetchfile'
+    end
+
+    context 'when given base-hostname argument' do
+      it 'should generate correct Tumblfetchfile'
+    end
+  end
+end
