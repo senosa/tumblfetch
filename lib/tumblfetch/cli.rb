0fc2f86b51bf8e021e11f1144fe08b6fac480d79
Sensuke Osawa <senosa@gmail.com>
2014-05-24 20:49:42 +0900

    Add module Tumblfetch for class CLI
    名前空間をきちんと使うということ。英語でうまく書けない。

--- lib/tumblfetch/cli.rb
+++ lib/tumblfetch/cli.rb
@@ -1,38 +1,40 @@
 require 'thor'
 require 'tumblfetch'
 
-class CLI < Thor
-  SETTINGS_EXIST_MSG =<<-EOS
+module Tumblfetch
+  class CLI < Thor
+    SETTINGS_EXIST_MSG =<<-EOS
 `.tumblfetch` already exists in this directory.
-  EOS
+    EOS
 
-  SETTINGS_GENERATED_MSG =<<-EOS
+    SETTINGS_GENERATED_MSG =<<-EOS
 `.tumblfetch` has been placed in this directory.
-  EOS
+    EOS
 
-  EXECUTE_TUMBLR_MSG =<<-EOS
+    EXECUTE_TUMBLR_MSG =<<-EOS
 `~/.tumblr` can't be found. Run `tumblr` for generating it.
 For details, see https://github.com/tumblr/tumblr_client#the-irb-console
-  EOS
+    EOS
 
-  desc 'version', 'Print a version'
-  def version
-    puts Tumblfetch::VERSION
-  end
+    desc 'version', 'Print a version'
+    def version
+      puts Tumblfetch::VERSION
+    end
 
-  desc 'init', 'Generate a .tumblfetch'
-  def init
-    if File.exist?(File.join(ENV['HOME'], '.tumblr'))
-      if File.exist?('./.tumblfetch')
-        $stderr.puts(SETTINGS_EXIST_MSG)
-      else
-        File.open('./.tumblfetch', 'w') do |file|
-          Tumblfetch.write_settings_template_to(file)
+    desc 'init', 'Generate a .tumblfetch'
+    def init
+      if File.exist?(File.join(ENV['HOME'], '.tumblr'))
+        if File.exist?('./.tumblfetch')
+          $stderr.puts(SETTINGS_EXIST_MSG)
+        else
+          File.open('./.tumblfetch', 'w') do |file|
+            Tumblfetch.write_settings_template_to(file)
+          end
+          puts(SETTINGS_GENERATED_MSG)
         end
-        puts(SETTINGS_GENERATED_MSG)
+      else
+        $stderr.puts(EXECUTE_TUMBLR_MSG)
       end
-    else
-      $stderr.puts(EXECUTE_TUMBLR_MSG)
     end
   end
 end
--- spec/lib/tumblfetch/cli_spec.rb
+++ spec/lib/tumblfetch/cli_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 require 'tumblfetch/cli'
 
-describe CLI do
+describe Tumblfetch::CLI do
   describe '#version' do
-    subject { capture(:stdout) { CLI.new.invoke('version') }.strip }
+    subject { capture(:stdout) { Tumblfetch::CLI.new.invoke('version') }.strip }
     it { should be_eql Tumblfetch::VERSION }
   end
 
@@ -11,28 +11,28 @@ describe CLI do
     context 'when ~/.tumblr is nonexistent' do
       it 'should print execute `tumblr`' do
         File.stub(:exist?).and_return(false)
-        msg = capture(:stderr) { CLI.new.invoke('init') }
-        expect(msg).to include CLI::EXECUTE_TUMBLR_MSG
+        msg = capture(:stderr) { Tumblfetch::CLI.new.invoke('init') }
+        expect(msg).to include Tumblfetch::CLI::EXECUTE_TUMBLR_MSG
       end
     end
 
     context 'when .tumblfetch is nonexistent' do
       it 'should generate a .tumblfetch' do
-        capture(:stdout) { CLI.new.invoke('init') }
+        capture(:stdout) { Tumblfetch::CLI.new.invoke('init') }
         File.exist?('./.tumblfetch').should be_true
       end
 
       it 'should print success message' do
-        msg = capture(:stdout) { CLI.new.invoke('init') }
-        expect(msg).to include CLI::SETTINGS_GENERATED_MSG
+        msg = capture(:stdout) { Tumblfetch::CLI.new.invoke('init') }
+        expect(msg).to include Tumblfetch::CLI::SETTINGS_GENERATED_MSG
       end
     end
 
     context 'when .tumblfetch already exist' do
       it 'should print warning message' do
         File.open('./.tumblfetch', 'w').close
-        msg = capture(:stderr) { CLI.new.invoke('init') }
-        expect(msg).to include CLI::SETTINGS_EXIST_MSG
+        msg = capture(:stderr) { Tumblfetch::CLI.new.invoke('init') }
+        expect(msg).to include Tumblfetch::CLI::SETTINGS_EXIST_MSG
       end
     end
   end
