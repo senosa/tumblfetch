30660dc9659c233d4694621de51cc5608f43975e
Sensuke Osawa <senosa@gmail.com>
2014-05-24 21:07:49 +0900

    Use subject.method instead of Tumblfetch::CLI.new.invoke('method')

--- spec/lib/tumblfetch/cli_spec.rb
+++ spec/lib/tumblfetch/cli_spec.rb
@@ -3,36 +3,38 @@ require 'tumblfetch/cli'
 
 describe Tumblfetch::CLI do
   describe '#version' do
-    subject { capture(:stdout) { Tumblfetch::CLI.new.invoke('version') }.strip }
-    it { should be_eql Tumblfetch::VERSION }
+    it 'should print correct version' do
+      output = capture(:stdout) { subject.version }
+      expect(output).to include Tumblfetch::VERSION
+    end
   end
 
   describe '#init' do
     context 'when ~/.tumblr is nonexistent' do
       it 'should print execute `tumblr`' do
         File.stub(:exist?).and_return(false)
-        msg = capture(:stderr) { Tumblfetch::CLI.new.invoke('init') }
-        expect(msg).to include Tumblfetch::CLI::EXECUTE_TUMBLR_MSG
+        output = capture(:stderr) { subject.init }
+        expect(output).to include Tumblfetch::CLI::EXECUTE_TUMBLR_MSG
       end
     end
 
     context 'when .tumblfetch is nonexistent' do
       it 'should generate a .tumblfetch' do
-        capture(:stdout) { Tumblfetch::CLI.new.invoke('init') }
+        output = capture(:stdout) { subject.init }
         File.exist?('./.tumblfetch').should be_true
       end
 
       it 'should print success message' do
-        msg = capture(:stdout) { Tumblfetch::CLI.new.invoke('init') }
-        expect(msg).to include Tumblfetch::CLI::SETTINGS_GENERATED_MSG
+        output = capture(:stdout) { subject.init }
+        expect(output).to include Tumblfetch::CLI::SETTINGS_GENERATED_MSG
       end
     end
 
     context 'when .tumblfetch already exist' do
       it 'should print warning message' do
         File.open('./.tumblfetch', 'w').close
-        msg = capture(:stderr) { Tumblfetch::CLI.new.invoke('init') }
-        expect(msg).to include Tumblfetch::CLI::SETTINGS_EXIST_MSG
+        output = capture(:stderr) { subject.init }
+        expect(output).to include Tumblfetch::CLI::SETTINGS_EXIST_MSG
       end
     end
   end
