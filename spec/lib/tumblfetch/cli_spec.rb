91069d1ac68e3fdef7a66a3fb015b837e715501b
Sensuke Osawa <senosa@gmail.com>
2014-05-25 16:12:36 +0900

    Extract stub to before block

--- spec/lib/tumblfetch/cli_spec.rb
+++ spec/lib/tumblfetch/cli_spec.rb
@@ -12,49 +12,53 @@ describe Tumblfetch::CLI, '#init' do
   let(:output) { capture(:stdout) { subject.init } }
   let(:dottumblr) { File.join(ENV['HOME'], '.tumblr') }
 
+  before do
+    File.stub(:exist?).with(dottumblr).and_return(dot_tumblr_exist)
+    File.stub(:exist?).with('.tumblfetch').and_return(dot_tumblfetch_exist)
+  end
+
   context 'when ~/.tumblr is nonexistent' do
+    let(:dot_tumblr_exist) { false }
+    let(:dot_tumblfetch_exist) { false }
+
     it 'should print execute `tumblr`' do
-      File.stub(:exist?).with(dottumblr).and_return(false)
       msg =  "`~/.tumblr` can't be found. Run `tumblr` for generating it.\n"
       msg << "For details, see https://github.com/tumblr/tumblr_client#the-irb-console"
       expect(output).to include msg
     end
 
     it 'should NOT generate a .tumblfetch' do
-      File.stub(:exist?).with(dottumblr).and_return(false)
       output
       FileTest.exist?('.tumblfetch').should be_false
     end
   end
 
   context 'when ~/.tumblr exist' do
+    let(:dot_tumblr_exist) { true }
+    
     context 'when .tumblfetch is nonexistent' do
+      let(:dot_tumblfetch_exist) { false }
+    
       it 'should generate a .tumblfetch' do
-        File.stub(:exist?).with(dottumblr).and_return(true)
-        File.stub(:exist?).with('.tumblfetch').and_return(false)
         output
         FileTest.exist?('.tumblfetch').should be_true
       end
 
       it 'should print success message' do
-        File.stub(:exist?).with(dottumblr).and_return(true)
-        File.stub(:exist?).with('.tumblfetch').and_return(false)
         msg = "`.tumblfetch` has been placed in this directory."
         expect(output).to include msg
       end
     end
 
     context 'when .tumblfetch already exist' do
+      let(:dot_tumblfetch_exist) { true }
+    
       it 'should print warning message' do
-        File.stub(:exist?).with(dottumblr).and_return(true)
-        File.stub(:exist?).with('.tumblfetch').and_return(true)
         msg = "`.tumblfetch` already exists in this directory."
         expect(output).to include msg
       end
 
       it 'should NOT generate a .tumblfetch' do
-        File.stub(:exist?).with(dottumblr).and_return(true)
-        File.stub(:exist?).with('.tumblfetch').and_return(true)
         output
         FileTest.exist?('.tumblfetch').should be_false
       end
