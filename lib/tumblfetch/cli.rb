ee089eaeb998970173c45a349dd833c503819d09
Sensuke Osawa <senosa@gmail.com>
2014-05-27 19:31:12 +0900

    Reduce nested if for more readable

--- lib/tumblfetch/cli.rb
+++ lib/tumblfetch/cli.rb
@@ -12,18 +12,19 @@ module Tumblfetch
 
     desc 'init', 'Generate a .tumblfetch'
     def init
-      if File.exist?(File.join(ENV['HOME'], '.tumblr'))
-        if File.exist?('.tumblfetch')
-          say "`.tumblfetch` already exists in this directory.", :red
-        else
-          File.open('.tumblfetch', 'w') do |file|
-            Tumblfetch.write_settings_template_to(file)
-          end
-          say "`.tumblfetch` has been placed in this directory.", :green
-        end
-      else
+      unless File.exist?(File.join(ENV['HOME'], '.tumblr'))
         say "`~/.tumblr` can't be found. Run `tumblr` for generating it.", :red
         say "For details, see https://github.com/tumblr/tumblr_client#the-irb-console", :red
+        return
+      end
+
+      if File.exist?('.tumblfetch')
+        say "`.tumblfetch` already exists in this directory.", :red
+      else
+        File.open('.tumblfetch', 'w') do |file|
+          Tumblfetch.write_settings_template_to(file)
+        end
+        say "`.tumblfetch` has been placed in this directory.", :green
       end
     end
   end
