ac94e451266d0b83395ecbaf72443ee16f93a1c6
Sensuke Osawa <senosa@gmail.com>
2014-06-08 22:47:29 +0900

    Fetcher has no references to ~/.tumblr fix #14

--- spec/lib/tumblfetch_spec.rb
+++ spec/lib/tumblfetch_spec.rb
@@ -2,138 +2,184 @@ require 'spec_helper'
 require 'tumblfetch'
 
 describe Tumblfetch::Fetcher, '#analyze' do
+  subject { @fetcher.analyze }
   before do
-    @f = Tumblfetch::Fetcher.new('dummy_conf')
-    @f.stub(:create_posts_list).and_return { [] }
+    @empty_array = []
+    Tumblr::Client.any_instance.stub(:posts).and_return { {'posts' => @empty_array} }
+    config = {'blog_name' => 'tt', 'last_fetch_id' => nil}
+    @fetcher = Tumblfetch::Fetcher.new(config)
   end
 
-  subject { @f.analyze }
   it { should be_a Hash }
-  it { should include :photos }
-  it { should include :posts }
+  it { should have_key :photos }
+  it { should have_key :posts }
+
+  describe '[:posts]' do
+    subject { @fetcher.analyze[:posts] }
+
+    it { should be_a Integer }
+
+    context 'when 2 posts and first fetch' do
+      before do
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 0})
+          .and_return do
+            {'posts' => [
+              {'id' => 123, 'photos' => @empty_array},
+              {'id' => 456, 'photos' => @empty_array}
+              ]
+            }
+          end
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 20})
+          .and_return { {'posts' => @empty_array} }
+      end
+      it { should eq 2 }
+    end
+
+    context 'when 1 New post in 3 posts' do
+      before do
+        @fetcher.config['last_fetch_id'] = :the_last
+        Tumblr::Client.any_instance.stub(:posts)
+          .and_return do
+            {'posts' => [
+              {'id' => 987, 'photos' => @empty_array},
+              {'id' => :the_last},
+              {'id' => 321}
+              ]
+            }
+          end
+      end
+      it { should eq 1 }
+    end
+
+    context 'when last fetch post is in second response' do
+      before do
+        @fetcher.config['last_fetch_id'] = :the_last
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 0})
+          .and_return do
+            {'posts' => [
+              {'id' => 99, 'photos' => @empty_array}
+              ]
+            }
+          end
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 20})
+          .and_return do
+            {'posts' => [
+              {'id' => 88, 'photos' => @empty_array},
+              {'id' => 77, 'photos' => @empty_array},
+              {'id' => :the_last}
+              ]
+            }
+          end
+      end
+      it { should eq 3 }
+    end
+  end
+
+  describe '[:photos]' do
+    subject { @fetcher.analyze[:photos] }
+    before do
+      Tumblfetch::Photo.stub(:new) { double 'photo' }
+      @fetcher.config['last_fetch_id'] = :the_last
+    end
+
+    it { should be_a Integer }
+
+    context 'when 1 photo in 1 post' do
+      before do
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 0})
+          .and_return do
+            {'posts' => [
+              {'id' => 123, 'photos' => ['photo1']},
+              {'id' => :the_last}
+              ]
+            }
+          end
+      end
+      it { should eq 1 }
+    end
+
+    context 'when 2 photos in 1 post(photoset)' do
+      before do
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 0})
+          .and_return do
+            {'posts' => [
+              {'id' => 123, 'photos' => ['photo1', 'photo2']},
+              {'id' => :the_last}
+              ]
+            }
+          end
+      end
+      it { should eq 2 }
+    end
+
+    context 'when 3 photos in 2 posts' do
+      before do
+        Tumblr::Client.any_instance.stub(:posts)
+          .with('tt', {:type => 'photo', :offset => 0})
+          .and_return do
+            {'posts' => [
+              {'id' => 123, 'photos' => ['photo1', 'photo2']},
+              {'id' => 456, 'photos' => ['photo3']},
+              {'id' => :the_last}
+              ]
+            }
+          end
+      end
+      it { should eq 3 }
+    end
+  end
+
 end
 
 describe Tumblfetch::Fetcher, '#download' do
+  subject { @fetcher.download }
   before do
-    @f = Tumblfetch::Fetcher.new('dummy_conf')
-    @p = double 'photo'
-    @p.stub(:download).and_return { ['success'] }
-    @f.photos = [@p]
+    @fetcher = Tumblfetch::Fetcher.new('conf')
+    @photo = double 'photo'
+    @photo.stub(:download).and_return { ['success'] }
+    @fetcher.photos = [@photo]
   end
 
-  subject { @f.download }
-  its([:success]) { should eq 1 }
-  its([:fails]) { should eq [] }
+  it { should be_a Hash }
+  it { should have_key :success }
+  it { should have_key :fails }
 
-  context 'when 2 photos in @photos' do
-    before { @f.photos << @p }
-    its([:success]) { should eq 2 }
-  end
+  describe '[:success]' do
+    subject { @fetcher.download[:success] }
 
-  context 'when Photo#download return fail' do
-    before { @p.stub(:download).and_return { 'fail' } }
+    it { should be_a Integer }
 
-    its([:success]) { should eq 0 }
-    its([:fails]) { should eq ['fail'] }
-  end
-end
-
-describe Tumblfetch::Fetcher, '#create_posts_list' do
-  before do
-    Tumblr::Client.any_instance.stub(:posts).and_return { {'posts' => []} }
-    config = {'blog_name' => 'tt', 'last_fetch_id' => nil}
-    @f = Tumblfetch::Fetcher.new(config)
-  end
-
-  subject { @f.send(:create_posts_list) }
-  it { should be_a Array }
-  it { should be_empty }
-
-  context 'when 2 posts and first fetch' do
-    before do
-      Tumblr::Client.any_instance.stub(:posts)
-        .with('tt', {:type => 'photo', :offset => 0})
-        .and_return do
-          {'posts' => [
-            {'id' => 123, 'photos' => []},
-            {'id' => 456, 'photos' => []}
-            ]
-          }
-        end
-      Tumblr::Client.any_instance.stub(:posts)
-        .with('tt', {:type => 'photo', :offset => 20})
-        .and_return { {'posts' => []} }
+    context 'when 2 photos all success' do
+      before { @fetcher.photos << @photo }
+      it { should eq 2 }
     end
-    it { should have(2).posts }
-  end
 
-  context 'when 1 New post in 3 posts' do
-    before do
-      @f.config['last_fetch_id'] = :the_last
-      Tumblr::Client.any_instance.stub(:posts)
-        .and_return do
-          {'posts' => [
-            {'id' => 987, 'photos' => []},
-            {'id' => :the_last},
-            {'id' => 321}
-            ]
-          }
-        end
+    context 'when Photo#download return fail' do
+      before { @photo.stub(:download).and_return { 'fail' } }
+      it { should eq 0 }
     end
-    it { should have(1).post }
   end
 
-  context 'when last fetch post is in second response' do
-    before do
-      @f.config['last_fetch_id'] = :the_last
-      Tumblr::Client.any_instance.stub(:posts)
-        .with('tt', {:type => 'photo', :offset => 0})
-        .and_return do
-          {'posts' => [
-            {'id' => 99, 'photos' => []}
-            ]
-          }
-        end
-      Tumblr::Client.any_instance.stub(:posts)
-        .with('tt', {:type => 'photo', :offset => 20})
-        .and_return do
-          {'posts' => [
-            {'id' => 88, 'photos' => []},
-            {'id' => 77, 'photos' => []},
-            {'id' => :the_last}
-            ]
-          }
-        end
+  describe '[:fails]' do
+    subject { @fetcher.download[:fails] }
+
+    it { should be_a Array }
+
+    context 'when 2 photos all success' do
+      before { @fetcher.photos << @photo }
+      it { should be_empty }
     end
-    it { should have(3).posts }
-  end
-end
 
-describe Tumblfetch::Fetcher, '#create_photos_list' do
-  before do
-    Tumblfetch::Photo.stub(:new) { double 'photo' }
-    @f = Tumblfetch::Fetcher.new('dummy_conf')
-    @posts = []
-  end
-
-  subject { @f.send(:create_photos_list, @posts) }
-  it { should be_a Array }
-
-  context 'when 1 photo in 1 post' do
-    before { @posts << {'id' => 987, 'photos' => ['photo1']} }
-    it { should have(1).photo }
-  end
-
-  context 'when 2 photos in 1 post(photoset)' do
-    before { @posts << {'id' => 987, 'photos' => ['photo1', 'photo2']} }
-    it { should have(2).photos }
-  end
-
-  context 'when 3 photos in 2 posts' do
-    before do
-      @posts <<  {'id' => 987, 'photos' => ['photo1']}
-      @posts <<  {'id' => 654, 'photos' => ['photo2', 'photo3']}
+    context 'when Photo#download return fail' do
+      before { @photo.stub(:download).and_return { 'fail' } }
+      it { should have(1).fails }
     end
-    it { should have(3).photos }
   end
+
 end
