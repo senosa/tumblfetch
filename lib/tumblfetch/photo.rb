7dcf22b75cecf9efe22ef746c789d88ad9d7d6b7
Sensuke Osawa <senosa@gmail.com>
2014-06-09 15:21:27 +0900

    Allow HTTP -> HTTPS redirection fix #12

--- lib/tumblfetch/photo.rb
+++ lib/tumblfetch/photo.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'fileutils'
 require 'fastimage'
+require 'open_uri_redirections'
 
 module Tumblfetch
   class Photo
@@ -23,7 +24,7 @@ module Tumblfetch
       result = []
       begin
         open(filename, "wb") do |file|
-          open(target_url) do |data|
+          open(target_url, :allow_redirections => :safe) do |data|
             file.write(data.read)
           end
         end
