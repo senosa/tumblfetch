b833ab6d9f0386f73831195a86e583c16fca8a02
Sensuke Osawa <senosa@gmail.com>
2014-06-11 12:31:48 +0900

    New spec more readable and easy adding testcase

--- lib/tumblfetch/photo.rb
+++ lib/tumblfetch/photo.rb
@@ -6,16 +6,28 @@ require 'open_uri_redirections'
 
 module Tumblfetch
   class Photo
-    attr_reader :post_id, :link_url, :photoset_idx, :original_width, :original_url
-    def initialize(post:, photoset_idx:)
-      @post_id = post['id']
-      @link_url = post['link_url']
-      @photoset_idx = post['photos'].size == 1 ? nil : photoset_idx
-      @original_width = post['photos'][photoset_idx]['original_size']['width']
-      @original_url = post['photos'][photoset_idx]['original_size']['url']
-      @alt_1_url = post['photos'][photoset_idx]['alt_sizes'][1]['url']
+    def initialize(post_id:, link_url:, photoset_idx:, original_url:, original_width:, alt_1_url:)
+      @post_id = post_id
+      @link_url = link_url
+      @photoset_idx = photoset_idx
+      @original_width = original_width
+      @original_url = original_url
+      @alt_1_url = alt_1_url
+      @target_url = nil
     end
 
+    def target_url
+      @target_url ||= strategy2
+    end
+
+    def filename
+      extname = File.extname(target_url)
+      filename = @post_id.to_s
+      filename << "_#{@photoset_idx}" if @photoset_idx
+      filename << extname
+    end
+
+    private
     def download
       # strategy1
       # target_url = link_url_is_real_original? ? @link_url : @original_url
@@ -57,7 +69,6 @@ module Tumblfetch
       target_url ||= @original_url
     end
 
-    private
     def link_url_is_real_original?
       return false unless @original_width == 1280
       return false unless @link_url =~ /.+(.jp(e)*g|.png|.gif)\z/
--- spec/lib/tumblfetch/photo_spec.rb
+++ spec/lib/tumblfetch/photo_spec.rb
@@ -1,2 +1,56 @@
 require 'spec_helper'
 require 'tumblfetch/photo'
+
+describe Tumblfetch::Photo do
+  let(:photo) do Tumblfetch::Photo.new(
+    post_id: post_id,
+    link_url: link_url,
+    photoset_idx: photoset_idx,
+    original_url: original_url,
+    original_width: original_width,
+    alt_1_url: alt_1_url
+    )
+  end
+  let(:post_id) { 1 }
+  let(:link_url) { nil }
+  let(:photoset_idx) { nil }
+  let(:original_url) { 'The_original_url' }
+  let(:original_width) { 500 }
+  let(:alt_1_url) { 'The_alt_1_url' }
+
+  describe '#target_url' do
+    subject { photo.target_url }
+
+    context 'when original_url is valid' do
+      let(:original_url) { 'http://www.snest.net/tumblfetch/under500.jpg' }
+
+      it { should include original_url }
+    end
+
+    context 'when link_url is valid' do
+      let(:link_url) { 'http://www.snest.net/tumblfetch/under500.jpg' }
+
+      it { should include link_url }
+    end
+
+    context 'when all targets are invalid' do
+      it { should include original_url }
+    end
+  end
+
+  describe '#filename' do
+    subject { photo.filename }
+
+    context 'when original_url is valid' do
+      let(:original_url) { 'http://www.snest.net/tumblfetch/under500.jpg' }
+
+      it { should include '1.jpg' }
+
+      context 'with photoset_idx' do
+        let(:photoset_idx) { 3 }
+
+        it { should include '1_3.jpg' }
+      end
+    end
+  end
+end
